b188dbd5b719916e2bd94c5c68dcb2b751350814
G.Montorio <Giulli@hotmail.de>
2020-03-04 15:02:17 +0100

    update

--- Simulink/ALF_Mecanum/mecanum_init.m
+++ Simulink/ALF_Mecanum/mecanum_init.m
@@ -4,4 +4,7 @@ r=0.1
 vx=0
 vy=0
 
-J=(1/r)*[1 1 a+b;1 -1 -(a+b);1 1 -(a+b);1 -1 a+b]
+J=(1/r)*[1 1 a+b;       %Vorne rechts
+        1 -1 -(a+b);    %Vorne links   
+        1 1 -(a+b);     %Hinten links
+        1 -1 a+b]       %Hinten rechts
