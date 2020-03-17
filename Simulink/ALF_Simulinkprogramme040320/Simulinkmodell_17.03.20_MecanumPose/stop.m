ee6553509a8167752f654dd2ef98703fc2c17205
G.Montorio <Giulli@hotmail.de>
2020-03-17 12:30:08 +0100

    Ordnung geschafft

--- Simulink/ALF_Simulinkprogramme040320/Simulinkmodell_17.03.20_MecanumPose/stop.m
+++
@@ -1,5 +0,0 @@
-%stopNode(r,'RALFCANread17b');                                              %RALFCANread17b_node wird gestoppt
-stopNode(r,'RALFCANsend');                                                 %RALFCANsend_node wird gestoppt
-stopNode(r,'RALFJoystick');                                                %RALFJoystick_node wird gestoppt
-stopNode(r,'joy');                                                         %joy_node wird gestartet
-c2=clock;
