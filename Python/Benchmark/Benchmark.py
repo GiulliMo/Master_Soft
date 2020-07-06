c32e1de51b14e911d6b28c27b6fc10d317d94ba6
GiulliMo <giulli@hotmail.de>
2020-07-06 15:52:42 +0200

    Benchmark fix

--- Python/Benchmark/Benchmark.py
+++ Python/Benchmark/Benchmark.py
@@ -3,20 +3,20 @@ import cv2
 
 
 class benchmark:
-    def __init__(self, filename):
-        self.image = cv2.imread(filename)
+    def __init__(self):
         self.detections = detections.detections()
 
-    def run(self):
+    def run(self, filename):
         c = 0
         while c <= 10:
-            self.detections.getdetectionsbycnn(self.image, "front")
-            self.detections.getdetectionsbyhog(self.image, "front")
-            self.detections.getdetectionsbytflite(self.image)
-            self.detections.getdetectionsbytfliteruntime(self.image)
+            imagehog, imagetfl, imagecf, imagetflr = cv2.imread(filename)
+            self.detections.getdetectionsbycnn(imagecf, "front")
+            self.detections.getdetectionsbyhog(imagehog, "front")
+            self.detections.getdetectionsbytflite(imagetfl)
+            self.detections.getdetectionsbytfliteruntime(imagetflr)
             c += 1
 
 
 if __name__ == '__main__':
-    benchmark = benchmark("standingpeople.jpg")
-    benchmark.run()
+    benchmark = benchmark()
+    benchmark.run("standingpeople.jpg")
