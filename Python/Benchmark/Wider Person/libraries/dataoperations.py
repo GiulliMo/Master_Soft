34098ab3a743b2bcfe494ae3fa850b8792b09a9f
Hannes Ditttmann <hannes.dittmann@t-online.de>
2020-07-29 09:48:49 +0200

    Wider Person Benchmark

---
+++ Python/Benchmark/Wider Person/libraries/dataoperations.py
@@ -0,0 +1,22 @@
+import pickle
+
+class dataoperations:
+    def __init__(self):
+        pass
+
+    def savedata(self, listofpersons):
+        with open('data.pkl', 'wb') as output:
+            pickle.dump(listofpersons, output, pickle.HIGHEST_PROTOCOL)
+
+    def getdata(self):
+        list = []
+        try:
+            with open('data.pkl', 'rb') as input:
+                list = pickle.load(input)
+
+        except EOFError:
+            print("Empty File")
+
+        for person in list:
+            print(person.name)
+        return list
